363052666ce6ad7f83ea68f562477561e24be2d9
Alexey Semenyuk <alexey.semenyuk@oracle.com>
2025-05-13 10:54:10 -0400

    Add a builder for LauncherStartupInfo with the unit tests.

---
+++ src/jdk.jpackage/share/classes/jdk/jpackage/internal/LauncherStartupInfoBuilder2.java
@@ -0,0 +1,246 @@
+/*
+ * Copyright (c) 2025, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package jdk.jpackage.internal;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+import java.util.jar.Attributes;
+import java.util.jar.JarFile;
+import java.util.jar.Manifest;
+import java.util.stream.Stream;
+import jdk.jpackage.internal.model.ConfigException;
+import jdk.jpackage.internal.model.LauncherJarStartupInfo;
+import jdk.jpackage.internal.model.LauncherJarStartupInfoMixin;
+import jdk.jpackage.internal.model.LauncherModularStartupInfo;
+import jdk.jpackage.internal.model.LauncherModularStartupInfoMixin;
+import jdk.jpackage.internal.model.LauncherStartupInfo;
+
+final class LauncherStartupInfoBuilder2 {
+
+    LauncherStartupInfo create() throws ConfigException {
+        return specifics.create();
+    }
+
+    LauncherStartupInfoBuilder2 inputDir(Path v) {
+        if (v != null) {
+            v.forEach(Objects::requireNonNull);
+        }
+        inputDir = v;
+        return this;
+    }
+
+    LauncherStartupInfoBuilder2 javaOptions(List<String> v) {
+        if (v != null) {
+            v.forEach(Objects::requireNonNull);
+        }
+        javaOptions = v;
+        return this;
+    }
+
+    LauncherStartupInfoBuilder2 defaultParameters(List<String> v) {
+        if (v != null) {
+            v.forEach(Objects::requireNonNull);
+        }
+        defaultParameters = v;
+        return this;
+    }
+
+    LauncherStartupInfoBuilder2 mainJar(Path v) {
+        ensureJarStartupInfo().mainJar = v;
+        return this;
+    }
+
+    LauncherStartupInfoBuilder2 mainClassName(String v) {
+        mainClassName = v;
+        return this;
+    }
+
+    LauncherStartupInfoBuilder2 predefinedRuntimeImage(Path v) {
+        ensureModuleStartupInfo().cookedRuntimePath = v;
+        return this;
+    }
+
+    LauncherStartupInfoBuilder2 moduleName(String v) {
+        ensureModuleStartupInfo().moduleName = v;
+        return this;
+    }
+
+    LauncherStartupInfoBuilder2 modulePath(List<Path> v) {
+        ensureModuleStartupInfo().modulePath = v;
+        return this;
+    }
+
+    private Path inputDir() {
+        return Optional.ofNullable(inputDir).orElse(Path.of(""));
+    }
+
+    private Optional<String> mainClassName() {
+        return Optional.ofNullable(mainClassName);
+    }
+
+    @SuppressWarnings("unchecked")
+    private <T extends LauncherStartupInfoBuilder> T ensureLauncherStartupInfo(Class<T> type, Supplier<T> ctor) {
+        if (!type.isInstance(specifics)) {
+            specifics = ctor.get();
+        }
+        return (T)specifics;
+    }
+
+    private JarStartupInfo ensureJarStartupInfo() {
+        return ensureLauncherStartupInfo(JarStartupInfo.class, JarStartupInfo::new);
+    }
+
+    private ModuleStartupInfo ensureModuleStartupInfo() {
+        return ensureLauncherStartupInfo(ModuleStartupInfo.class, ModuleStartupInfo::new);
+    }
+
+    private LauncherStartupInfo createLauncherStartupInfo(String mainClassName, List<Path> classpath) {
+        Objects.requireNonNull(mainClassName);
+        classpath.forEach(Objects::requireNonNull);
+        return new LauncherStartupInfo.Stub(mainClassName,
+                Optional.ofNullable(javaOptions).orElseGet(List::of),
+                Optional.ofNullable(defaultParameters).orElseGet(List::of),
+                classpath);
+    }
+
+    private static List<Path> createClasspath(Path inputDir, Set<Path> excludes) {
+        excludes.forEach(Objects::requireNonNull);
+        try (final var walk = Files.walk(inputDir)) {
+            return walk.filter(Files::isRegularFile)
+                    .filter(file -> file.getFileName().toString().endsWith(".jar"))
+                    .map(inputDir::relativize)
+                    .filter(Predicate.not(excludes::contains))
+                    .distinct()
+                    .toList();
+        } catch (IOException ex) {
+            throw new UncheckedIOException(ex);
+        }
+    }
+
+    private final class ModuleStartupInfo implements LauncherStartupInfoBuilder {
+
+        @Override
+        public LauncherStartupInfo create() throws ConfigException {
+            final var fullModulePath = getFullModulePath();
+
+            // Try to find the module in the specified module path list.
+            final var moduleInfo = JLinkRuntimeBuilder.createModuleFinder(fullModulePath).find(moduleName)
+                    .map(ModuleInfo::fromModuleReference).or(() -> {
+                        // Failed to find the module in the specified module path list.
+                        return cookedRuntimePath().flatMap(cookedRuntime -> {
+                            // Lookup the module in the external runtime.
+                            return ModuleInfo.fromCookedRuntime(moduleName, cookedRuntime);
+                        });
+                    }).orElseThrow(() -> {
+                        return I18N.buildConfigException("error.no-module-in-path", moduleName).create();
+                    });
+
+            final var effectiveMainClassName = mainClassName().or(moduleInfo::mainClass).orElseThrow(() -> {
+                return I18N.buildConfigException("ERR_NoMainClass").create();
+            });
+
+            // If module is located in the file system, exclude it from the classpath.
+            final var classpathExcludes = moduleInfo.fileLocation().filter(moduleFile -> {
+                return moduleFile.startsWith(inputDir());
+            }).map(inputDir()::relativize).map(Set::of).orElseGet(Set::of);
+
+            return LauncherModularStartupInfo.create(
+                    createLauncherStartupInfo(effectiveMainClassName, createClasspath(inputDir(), classpathExcludes)),
+                    new LauncherModularStartupInfoMixin.Stub(moduleInfo.name()));
+        }
+
+        private List<Path> getFullModulePath() {
+            return cookedRuntimePath().map(runtimeImage -> {
+                return Stream.of(modulePath(), List.of(runtimeImage.resolve("lib"))).flatMap(List::stream).toList();
+            }).orElse(modulePath());
+        }
+
+        private List<Path> modulePath() {
+            return Optional.ofNullable(modulePath).orElseGet(List::of);
+        }
+
+        private Optional<Path> cookedRuntimePath() {
+            return Optional.ofNullable(cookedRuntimePath);
+        }
+
+        private String moduleName;
+        private List<Path> modulePath;
+        private Path cookedRuntimePath;
+    }
+
+    private final class JarStartupInfo implements LauncherStartupInfoBuilder {
+
+        @Override
+        public LauncherStartupInfo create() throws ConfigException {
+            final var mainJarPath = inputDir().resolve(mainJar);
+
+            if (!Files.exists(mainJarPath)) {
+                throw I18N.buildConfigException()
+                        .message("error.main-jar-does-not-exist", mainJar)
+                        .advice("error.main-jar-does-not-exist.advice")
+                        .create();
+            }
+
+            final var effectiveMainClassName = mainClassName().or(() -> {
+                try (final var jf = new JarFile(mainJarPath.toFile())) {
+                    return Optional.ofNullable(jf.getManifest()).map(Manifest::getMainAttributes).map(attrs -> {
+                        return attrs.getValue(Attributes.Name.MAIN_CLASS);
+                    });
+                } catch (IOException ex) {
+                    throw new UncheckedIOException(ex);
+                }
+            }).orElseThrow(() -> {
+                return I18N.buildConfigException()
+                        .message("error.no-main-class-with-main-jar", mainJar)
+                        .advice("error.no-main-class-with-main-jar.advice", mainJar)
+                        .create();
+            });
+
+            return LauncherJarStartupInfo.create(
+                    createLauncherStartupInfo(effectiveMainClassName, createClasspath(inputDir(), Set.of(mainJar))),
+                    new LauncherJarStartupInfoMixin.Stub(mainJar, mainClassName().isEmpty()));
+        }
+
+        private Path mainJar;
+    }
+
+    private interface LauncherStartupInfoBuilder {
+        LauncherStartupInfo create() throws ConfigException;
+    }
+
+    private Path inputDir;
+    private String mainClassName;
+    private List<String> javaOptions;
+    private List<String> defaultParameters;
+    private LauncherStartupInfoBuilder specifics;
+}
