4d31902af0698d10a4f8f6af8e0ec341a9ba4d7e
Alexey Semenyuk <alexey.semenyuk@oracle.com>
2025-10-22 14:39:33 -0400

    PropertyFile: import clean up

--- test/jdk/tools/jpackage/helpers/jdk/jpackage/test/PropertyFile.java
+++ test/jdk/tools/jpackage/helpers/jdk/jpackage/test/PropertyFile.java
@@ -32,7 +32,6 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.Properties;
-import java.util.function.Supplier;
 
 public final class PropertyFile {
 
