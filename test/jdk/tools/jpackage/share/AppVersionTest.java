870b0c967e7372ecbcea54a49d93381c50b868bc
Alexey Semenyuk <alexey.semenyuk@oracle.com>
2026-03-30 19:51:34 -0400

    8381367: [lworld] tools/jpackage/share/AppVersionTest.java timed out while reporting errors

--- test/jdk/tools/jpackage/share/AppVersionTest.java
+++ test/jdk/tools/jpackage/share/AppVersionTest.java
@@ -70,7 +70,7 @@ import jdk.jpackage.test.TKit;
  * @library /test/jdk/tools/jpackage/helpers
  * @build jdk.jpackage.test.*
  * @compile -Xlint:all -Werror AppVersionTest.java
- * @run main/othervm/timeout=1440 -Xmx512m jdk.jpackage.test.Main
+ * @run main/othervm/timeout=2880 -Xmx512m jdk.jpackage.test.Main
  *  --jpt-run=AppVersionTest
  */
 
@@ -988,9 +988,6 @@ public final class AppVersionTest {
                         }));
                     });
                     yield MacBundle.fromPath(predefinedRuntimeDir).orElseThrow().homeDir();
-                }
-                default -> {
-                    throw new AssertionError();
                 }
             };
 
